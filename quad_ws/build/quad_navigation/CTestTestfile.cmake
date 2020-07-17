6fc98f38d47a23510d346eb413ca4911de3ee507
Dinesh Thapa Magar <dineshtm10@gmail.com>
2020-07-17 23:07:23 +0545

    commit

--- quad_ws/build/quad_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/ROS_Beginner/quad_ws/src/quad_navigation
-# Build directory: /home/user/ROS_Beginner/quad_ws/build/quad_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
